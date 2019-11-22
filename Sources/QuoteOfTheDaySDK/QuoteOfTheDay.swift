bf88dcc3aa00e26f2690452c5895f5dbc7549e47
Stewart Thomson <stewart.thomson@bemo.co>
2019-11-22 16:07:52 +0000

    Updated structure

---
+++ Sources/QuoteOfTheDaySDK/QuoteOfTheDay.swift
@@ -0,0 +1,15 @@
+//
+//  QuoteOfTheDay.swift
+//  QuoteOfTheDaySDK
+//
+//  Created by Stewart Thomson on 22/11/2019.
+//  Copyright © 2019 Stewart Thomson. All rights reserved.
+//
+
+import Foundation
+
+open class QuoteOfTheDay {
+    open class func qod() -> String {
+        return "Some inspiration"
+    }
+}
