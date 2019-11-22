6f363d74cf8b223c8405ec9dbffe8d0ce75a93f3
Stewart Thomson <stewart.thomson@bemo.co>
2019-11-22 15:07:37 +0000

    Initial Commit

---
+++ QuoteOfTheDaySDK/QuoteOfTheDaySDK.h
@@ -0,0 +1,19 @@
+//
+//  QuoteOfTheDaySDK.h
+//  QuoteOfTheDaySDK
+//
+//  Created by Stewart Thomson on 22/11/2019.
+//  Copyright © 2019 Stewart Thomson. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for QuoteOfTheDaySDK.
+FOUNDATION_EXPORT double QuoteOfTheDaySDKVersionNumber;
+
+//! Project version string for QuoteOfTheDaySDK.
+FOUNDATION_EXPORT const unsigned char QuoteOfTheDaySDKVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <QuoteOfTheDaySDK/PublicHeader.h>
+
+
